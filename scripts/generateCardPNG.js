ccdc4b1d6fee3d485bcd8b9fc8231988b2bd224c
Amy Tan <amy.tan.su@gmail.com>
2023-06-11 11:09:17 -0400

    added detail comments to function

--- scripts/generateCardPNG.js
+++ scripts/generateCardPNG.js
@@ -3,5 +3,10 @@ function GeneratePNG(){
     var inputFile = document.getElementById('fileInput').value;
     console.log(inputFile);
     console.log("GeneratePNG Completed")
+    // Default Values
+    /*  Generate Number Visuals == True
+        Qnty == 1 */
+
+    // TODO: Create a checkbox to ask for user input on Generating Number Visuals 
 
 }
